ce892c57d47f36f303cc7fa61b0f549ca2da4010
nainapatil27 <nainapatil2707@gmail.com>
2024-08-01 12:52:41 +0530

    bubblesort

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+    int i;
+    int num;
+    int fact;
+    printf("enter number:");
+    scanf("%d,&num");
+    for(i=i;i<=num;i++)
+  {
+     
+    fact=(fact*i)*i;
+    printf('factorial of: %d',fact);
+return;
+   } 
+    getch();
+}
